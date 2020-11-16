fcbb06a0efa9f70c517dc412c197cc591daa9173
taisa-asiato <taisa.aikou@gmail.com>
2020-11-16 23:42:24 +0900

    [MOD]lib.h update

--- os/lib.h
+++ os/lib.h
@@ -1,10 +1,6 @@
-#ifndef _LIB_H_INCLUDE_
-#define _LIB_H_INCLUDE_
+#ifndef _LIB_H_INCLUDED_
+#define _LIB_H_INCLUDED_
 
-int putc(unsigned char c);	/* 1文字送信 */
-unsigned char getc(void);	/* 1文字受信 */
-int puts(unsigned char * str);	/* 文字列送信 */
-int gets(unsigned char * buf);	/* 文字列受信 */
 void *memset(void *b, int c, long len); /* メモリを特定のバイトで埋める */
 void *memcpy(void *dst, const void *src, long len); /* メモリのコピー */
 int memcmp(const void *b1, const void *b2, long len); /* メモリの比較を行う */
@@ -12,7 +8,12 @@ int strlen(const char *s); /* 文字列の長さを返す */
 char *strcpy(char *dst, const char *src); /* 文字列のコピーを行う */
 int strcmp(const char *s1, const char *s2); /* 文字列の比較 */
 int strncmp(const char *s1, const char *s2, int len);
+
+int putc(unsigned char c);	/* 1文字送信 */
+unsigned char getc(void);	/* 1文字受信 */
+int puts(unsigned char * str);	/* 文字列送信 */
+int gets(unsigned char * buf);	/* 文字列受信 */
 int putxval(unsigned long value, int column);
-int func(int a, int b); /* 実験用 */
+// int func(int a, int b); /* 実験用 */
 
 #endif
